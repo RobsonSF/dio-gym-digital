df8da560cddc4a46780dd6fe90813978cb1b0f1d
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-04 17:11:00 -0300

    create Aluno

---
+++ src/main/java/com/github/robsonsf/diogymdigital/entities/Aluno.java
@@ -0,0 +1,37 @@
+package com.github.robsonsf.diogymdigital.entities;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "tb_alunos")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+public class Aluno {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String nome;
+
+  @Column(unique = true)
+  private String cpf;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+}
