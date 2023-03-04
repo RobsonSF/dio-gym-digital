a89808f594f8fc0a184b6575e6a92a97d9af0446
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-04 17:21:23 -0300

    create AlunoUpdateForm

---
+++ src/main/java/com/github/robsonsf/diogymdigital/entities/form/AlunoUpdateForm.java
@@ -0,0 +1,12 @@
+package com.github.robsonsf.diogymdigital.entities.form;
+
+import java.time.LocalDate;
+
+public class AlunoUpdateForm {
+
+  private String nome;
+
+  private String bairro;
+
+  private LocalDate dataDeNascimento;
+}
