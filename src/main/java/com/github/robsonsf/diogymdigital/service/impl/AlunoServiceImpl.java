3fde1fe62d9ccfd5541a2b64c5b76d7703289c2b
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-04 18:35:46 -0300

    AlunoServiceImpl implements IAlunoService

--- src/main/java/com/github/robsonsf/diogymdigital/service/impl/AlunoServiceImpl.java
+++ src/main/java/com/github/robsonsf/diogymdigital/service/impl/AlunoServiceImpl.java
@@ -1,5 +1,49 @@
 package com.github.robsonsf.diogymdigital.service.impl;
 
-public class AlunoServiceImpl {
+import java.util.List;
+
+import com.github.robsonsf.diogymdigital.entities.Aluno;
+import com.github.robsonsf.diogymdigital.entities.AvaliacaoFisica;
+import com.github.robsonsf.diogymdigital.entities.form.AlunoForm;
+import com.github.robsonsf.diogymdigital.entities.form.AlunoUpdateForm;
+import com.github.robsonsf.diogymdigital.service.IAlunoService;
+
+public class AlunoServiceImpl implements IAlunoService {
+
+	@Override
+	public Aluno create(AlunoForm form) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Aluno get(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Aluno> getAll(String dataDeNascimento) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Aluno update(Long id, AlunoUpdateForm formUpdate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(Long id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<AvaliacaoFisica> getAllAvaliacaoFisicaId(Long id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
