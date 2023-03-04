c2e63b8a80bc61399d92924677d4a289ccd2e16b
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-04 18:10:51 -0300

    create AvaliacaoFisicaRepository

---
+++ src/main/java/com/github/robsonsf/diogymdigital/repositories/AvaliacaoFisicaRepository.java
@@ -0,0 +1,10 @@
+package com.github.robsonsf.diogymdigital.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.github.robsonsf.diogymdigital.entities.Aluno;
+
+@Repository
+public interface AvaliacaoFisicaRepository extends JpaRepository<Aluno, Long>{
+}
