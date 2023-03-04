d3d63bca5bf000ee65de17e744fd10e7a5ad7c76
Robson S Fernandes <rbsn.dev@gmail.com>
2023-03-04 16:59:38 -0300

    create spring boot project

---
+++ src/main/java/com/github/robsonsf/diogymdigital/DioGymDigitalApplication.java
@@ -0,0 +1,13 @@
+package com.github.robsonsf.diogymdigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioGymDigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioGymDigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/robsonsf/diogymdigital/DioGymDigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.robsonsf.diogymdigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioGymDigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
